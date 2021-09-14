04a6e8d56e0005cb0300701ea303e5225b55686a
Henry Doan <henrydoan96@gmail.com>
2021-09-13 20:03:24 -0600

    user stuff almost done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # root routes
+  root "accounts#index"
+  devise_for :users # from devise for user routes
+
+  # not connect to the user and continue with 
+  # parent and child route pattern from here
+  resources :accounts do
+    resources :items
+  end
 end
